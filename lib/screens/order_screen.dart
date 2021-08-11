a9e527f660e8506d7c5b2ce2e02a44337759da9b
mohmedkasim <mk4957454@gmail.com>
2021-08-10 18:56:15 -1200

    init files

---
+++ lib/screens/order_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class OrderScreen extends StatefulWidget {
+  static const String id = "order_screen";
+  @override
+  _OrderScreenState createState() => _OrderScreenState();
+}
+
+class _OrderScreenState extends State<OrderScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
