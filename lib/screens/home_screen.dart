8bb90a9640f3df11aba29ba543154e2fa1fcb989
mohmedkasim <mk4957454@gmail.com>
2021-08-12 13:17:48 -1200

    add ConatinerTopWeek

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -45,51 +45,7 @@ class _HomeScreenState extends State<HomeScreen> {
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 20),
-            Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.blue[300]),
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        Icon(
-                          FontAwesomeIcons.solidStar,
-                          size: 10,
-                        ),
-                        SizedBox(
-                          width: 5,
-                        ),
-                        Text('5.0'),
-                      ],
-                    ),
-                    SizedBox(height: 15),
-                    Text(
-                      'Top of the week',
-                      style: TextStyle(fontSize: 10),
-                    ),
-                    SizedBox(height: 5),
-                    Text(
-                      'Party cake',
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 13),
-                    CircleAvatar(
-                      backgroundColor: Colors.white,
-                      child: Icon(
-                        Icons.arrow_forward,
-                        color: Colors.black,
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
+            ContainerTopWeek(),
             SizedBox(height: 40),
             Text(
               'Popular',
@@ -130,6 +86,74 @@ class _HomeScreenState extends State<HomeScreen> {
           ],
         ),
       )),
+    );
+  }
+}
+
+class ContainerTopWeek extends StatelessWidget {
+  const ContainerTopWeek({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 30.0),
+          child: Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.orange[300]),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Icon(
+                        FontAwesomeIcons.solidStar,
+                        size: 10,
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Text('5.0'),
+                    ],
+                  ),
+                  SizedBox(height: 15),
+                  Text(
+                    'Top of the week',
+                    style: TextStyle(fontSize: 10),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    'Party cake',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 13),
+                  CircleAvatar(
+                    backgroundColor: Colors.white,
+                    child: Icon(
+                      Icons.arrow_forward,
+                      color: Colors.black,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          right: 20,
+          bottom: 1,
+          child: Image.asset(
+            'assets/images/girl_party.png',
+            width: MediaQuery.of(context).size.width / 4,
+          ),
+        )
+      ],
     );
   }
 }
