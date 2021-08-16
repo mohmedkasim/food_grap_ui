f03904a447b562998b7a0942567314402c0686b3
mohmedkasim <mk4957454@gmail.com>
2021-08-15 19:11:35 -1200

    addition OrderScreen
    add first phase of screen

--- lib/screens/order_screen.dart
+++ lib/screens/order_screen.dart
@@ -1,4 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:food_grab/Components/logo_container.dart';
+import 'package:food_grab/Components/rounded_container.dart';
+import 'package:food_grab/Components/user_account_container.dart';
+import 'package:food_grab/constents.dart';
 
 class OrderScreen extends StatefulWidget {
   static const String id = "order_screen";
@@ -9,6 +15,158 @@ class OrderScreen extends StatefulWidget {
 class _OrderScreenState extends State<OrderScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+      child: Scaffold(
+          body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: 350,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 15.0, left: 15.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        LogoContainer(),
+                        SizedBox(height: 40),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Now Your",
+                              style: TextStyle(
+                                  fontSize: 30, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(height: 4),
+                            Text(
+                              "order",
+                              style: TextStyle(
+                                  fontSize: 25, fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 40),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            RoundedContainer(
+                              number: "2x",
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Pad Thai",
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  "Thai mcdonald",
+                                  style: TextStyle(color: Colors.grey),
+                                ),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "Total: \$",
+                                      style: TextStyle(color: Colors.grey),
+                                    ),
+                                    SizedBox(
+                                      width: 2,
+                                    ),
+                                    Text(
+                                      "36",
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold),
+                                    )
+                                  ],
+                                )
+                              ],
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                    decoration: BoxDecoration(
+                      borderRadius:
+                          BorderRadius.only(bottomLeft: Radius.circular(15)),
+                      color: secondaryColor,
+                    ),
+                    width: MediaQuery.of(context).size.width / 3,
+                    height: 350,
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 15.0, bottom: 15),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          UserContainer(),
+                          SizedBox(height: 10),
+                          Text(
+                            'In Progress:',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(30),
+                            child: Container(
+                              color: Colors.white,
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Image.asset(
+                                  'assets/images/mcdonalds.png',
+                                  width: 30,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Divider(
+                            color: Colors.grey,
+                            endIndent: 20,
+                            indent: 20,
+                          ),
+                          Text('Last order: ',
+                              style: TextStyle(fontWeight: FontWeight.bold)),
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(100),
+                            child: Container(
+                              color: Colors.white,
+                              child: Padding(
+                                padding: const EdgeInsets.only(
+                                    top: 10.0,
+                                    bottom: 10.0,
+                                    right: 5.0,
+                                    left: 5.0),
+                                child: Image.asset(
+                                  'assets/images/meiji.jpeg',
+                                  width: 40,
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ))
+              ],
+            ),
+          ],
+        ),
+      )),
+    );
   }
 }
