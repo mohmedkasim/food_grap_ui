a4a50406b628fb369fb8f45890020f63826a860d
mohmedkasim <mk4957454@gmail.com>
2021-08-15 16:32:42 -1200

    add onPress

--- lib/Components/popular_item.dart
+++ lib/Components/popular_item.dart
@@ -7,6 +7,7 @@ class PopularItemList extends StatelessWidget {
   final String price;
   final String percent;
   final String image;
+  final Function onPressed;
 
   const PopularItemList(
       {Key key,
@@ -14,70 +15,74 @@ class PopularItemList extends StatelessWidget {
       this.category,
       this.price,
       this.percent,
-      this.image})
+      this.image,
+      this.onPressed})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Container(
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15), color: Colors.white),
-        child: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ClipRRect(
-                  borderRadius: BorderRadius.circular(15),
-                  child: Image.asset(
-                    image,
-                    width: 130,
-                    height: 180,
-                    fit: BoxFit.fitHeight,
-                  )),
-              SizedBox(height: 10),
-              Text(
-                title ?? 'cake name',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 2),
-              Row(
-                children: [
-                  Text(
-                    '${category ?? "category"} - ${price ?? "price"}',
-                    style: TextStyle(color: Colors.grey[400]),
-                  ),
-                  Icon(
-                    FontAwesomeIcons.dollarSign,
-                    size: 10,
-                    color: Colors.grey[400],
-                  )
-                ],
-              ),
-              SizedBox(
-                height: 7,
-              ),
-              Row(
-                children: [
-                  Icon(
-                    FontAwesomeIcons.smile,
-                    color: Colors.green[700],
-                    size: 15,
-                  ),
-                  SizedBox(width: 5),
-                  Text(
-                    '${percent ?? "-"}%',
-                    style: TextStyle(
-                      color: Colors.green[700],
-                      fontSize: 15,
+      child: GestureDetector(
+        onTap: onPressed,
+        child: Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15), color: Colors.white),
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: Image.asset(
+                      image,
+                      width: 130,
+                      height: 180,
+                      fit: BoxFit.fitHeight,
+                    )),
+                SizedBox(height: 10),
+                Text(
+                  title ?? 'cake name',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 2),
+                Row(
+                  children: [
+                    Text(
+                      '${category ?? "category"} - ${price ?? "price"}',
+                      style: TextStyle(color: Colors.grey[400]),
                     ),
-                  ),
-                ],
-              )
-            ],
+                    Icon(
+                      FontAwesomeIcons.dollarSign,
+                      size: 10,
+                      color: Colors.grey[400],
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 7,
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      FontAwesomeIcons.smile,
+                      color: Colors.green[700],
+                      size: 15,
+                    ),
+                    SizedBox(width: 5),
+                    Text(
+                      '${percent ?? "-"}%',
+                      style: TextStyle(
+                        color: Colors.green[700],
+                        fontSize: 15,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
